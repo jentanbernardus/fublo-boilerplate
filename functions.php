9af710df1e9226948ea47f41af88c9f44727bdf9
Oliver Caldwell <oliver@flowdev.co.uk>
2011-08-25 10:27:43 +0100

    Swapped the 960 inclusion for skeleton

--- functions.php
+++ functions.php
@@ -20,16 +20,14 @@ function fublo_boilerplate_init()
 		$template_url = get_bloginfo('template_url');
 		
 		// Register styles
-		wp_register_style('fublo_boilerplate_reset', $template_url . '/960/code/css/min/reset.css');
-		wp_register_style('fublo_boilerplate_text', $template_url . '/960/code/css/min/text.css');
-		wp_register_style('fublo_boilerplate_960', $template_url . '/960/code/css/min/960.css');
-		wp_register_style('fublo_boilerplate_960_responsive', $template_url . '/960-responsive/style.css');
+		wp_register_style('fublo_boilerplate_base', $template_url . '/skeleton/stylesheets/base.css');
+		wp_register_style('fublo_boilerplate_skeleton', $template_url . '/skeleton/stylesheets/skeleton.css');
+		wp_register_style('fublo_boilerplate_layout', $template_url . '/skeleton/stylesheets/layout.css');
 		
 		// Enqueue styles
-		wp_enqueue_style('fublo_boilerplate_reset');
-		wp_enqueue_style('fublo_boilerplate_text');
-		wp_enqueue_style('fublo_boilerplate_960');
-		wp_enqueue_style('fublo_boilerplate_960_responsive');
+		wp_enqueue_style('fublo_boilerplate_base');
+		wp_enqueue_style('fublo_boilerplate_skeleton');
+		wp_enqueue_style('fublo_boilerplate_layout');
 		
 		// Register scripts
 		wp_register_script('fublo_boilerplate_mootools', $template_url . '/mootools.js', false, '1.3.2', true);
