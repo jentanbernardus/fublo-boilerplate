c4a8e0b76e255bdec902c6ab0cf6c6d8d49e780b
Oliver Caldwell <oliver@flowdev.co.uk>
2011-09-16 11:37:06 +0100

    Swapped out the title tag for something simpler, gives you more control

--- header-fublo-boilerplate.php
+++ header-fublo-boilerplate.php
@@ -1,17 +1,7 @@
 <!DOCTYPE html>
 <html lang='en'>
 	<head>
-		<title><?php
-			wp_title('|', true, 'right');
-			bloginfo('name');
-			
-			$site_description = get_bloginfo('description', 'display');
-			
-			if ($site_description && (is_home() || is_front_page()))
-			{
-				echo ' | ' . $site_description;
-			}
-		?></title>
+		<title><?php the_title(); ?></title>
 		<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
 		<!--[if lt IE 9]><script type='text/javascript' src='<?php bloginfo('template_url'); ?>/html5.php'></script><![endif]-->
 		<?php wp_head(); ?>
