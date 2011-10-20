c3b46940a9e44fb1e01f2c3e77ab4ee219a24ddd
James Cooke <james@fublo.net>
2011-10-20 19:34:09 +0100

    Patching back wp-title() into the head title field a la twentyeleven style. Control can be recovered by filtering on 'wp_title'.

--- header-fublo-boilerplate.php
+++ header-fublo-boilerplate.php
@@ -1,7 +1,27 @@
 <!DOCTYPE html>
 <html lang='en'>
 	<head>
-		<title><?php the_title(); ?></title>
+		<title><?php
+	/*
+	 * Print the <title> tag based on what is being viewed.
+	 */
+	global $page, $paged;
+
+	wp_title( '|', true, 'right' );
+
+	// Add the blog name.
+	bloginfo( 'name' );
+
+	// Add the blog description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		echo " | $site_description";
+
+	// Add a page number if necessary:
+	if ( $paged >= 2 || $page >= 2 )
+		echo ' | ' . sprintf( __( 'Page %s', 'fublo-boilerplate' ), max( $paged, $page ) );
+
+	?></title>
 		<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
 		<!--[if lt IE 9]><script type='text/javascript' src='<?php bloginfo('template_url'); ?>/assets/javascript/html5.js'></script><![endif]-->
 		<?php wp_head(); ?>
